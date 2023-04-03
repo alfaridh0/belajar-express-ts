914796ebe05d7f3b76cc1225989ee0002eeb5e0e
Niken Alfrido <alfrisiboro@gmail.com>
2023-04-03 16:56:37 +0700

    My first commit bitch

---
+++ src/controllers/TodoController.ts
@@ -0,0 +1,55 @@
+import {Request, Response} from "express";
+import IController from "./ControllerInterface";
+import TodoService from "../services/TodoService";
+
+
+
+class TodoController implements IController {
+    index = async (req: Request, res: Response): Promise<Response> => {
+        const service: TodoService = new TodoService(req);
+        const todos = await service.getAll();
+        return res.send({
+            data: todos,
+            message: ""
+        }); 
+    }
+
+    create = async (req: Request, res: Response): Promise<Response> => {
+        const service: TodoService = new TodoService(req);
+        const todos = await service.store();
+        return res.send({
+            data: todos,
+            message: "Todo Created"
+        });
+    }
+
+    show = async (req: Request, res: Response): Promise<Response> => {  
+        const service: TodoService = new TodoService(req);
+        const todo = await service.getOne();
+        return res.send({
+            data: todo,
+            message: ""
+        });
+    }
+
+    update = async (req: Request, res: Response): Promise<Response> => {
+        const service: TodoService = new TodoService(req);
+        const todo = await service.update();
+        return res.send({
+            data: todo,
+            message: "Update Sukses"
+        });
+    }
+
+    delete = async (req: Request, res: Response): Promise<Response> => {
+        const service: TodoService = new TodoService(req);
+        const todo = await service.delete();
+        return res.send({
+            data: todo,
+            message: "Todo Deleted"
+        });
+    }
+
+}
+
+export default new TodoController();
---
+++ src/controllers/UserController.ts
@@ -0,0 +1,49 @@
+import {Request, Response} from "express";
+import IController from "./ControllerInterface";
+
+
+let data: any[] = [
+    { id:1, name: "Adi" },
+    { id:2, name: "Budi" },
+    { id:3, name: "Cidi" },
+    { id:4, name: "Xdi" },
+    { id:5, name: "Fivedi" },
+]; 
+
+class UserController implements IController {
+    index(req: Request, res: Response): Response {
+        return res.send(data); 
+    }
+
+    create(req: Request, res: Response): Response {
+        const {id, name} = req.body;
+
+        data.push({ id,name });
+        return res.send('create sukses')
+    }
+
+    show(req: Request, res: Response): Response {
+        const { id } = req.params;
+
+        let person = data.find(item => item.id == id);
+        return res.send(person);
+    }
+
+    update(req: Request, res: Response): Response {
+        const { id } = req.params;
+        const { name } = req.body;
+
+        let person = data.find(item => item.id == id);
+        person.name = name;
+        return res.send("Update Sukses");
+    }
+
+    delete(req: Request, res: Response): Response {
+        const { id } = req.params as {id: string}
+        let people = data.filter(item => item.id != id)
+        return res.send(people);
+    }
+
+}
+
+export default new UserController();
